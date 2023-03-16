ec54003805308baa673a8833a15b2d0a846e3602
suletom <suletom@freemail.hu>
2023-03-16 16:57:28 +0100

    watchdog....

--- modules/watchdog.js
+++ modules/watchdog.js
@@ -1,10 +1,18 @@
 const notifier = require("./modules/notifier.js");
 
-const watchdog = function(configobj,currentdata){
+class watchdog { 
 
-    let errors=[];
+    constructor() {
+        this.errors=[];
+        this.param_missing=false;
+    }
 
-    function param_ok(param,data){
+    run(configobj,currentdata,watch){
+
+        
+    }
+
+    param_ok(param,data){
 
         //check if exists
         if (data[param]!==undefined){
@@ -13,6 +21,7 @@ const watchdog = function(configobj,currentdata){
                 return true;
             }else{
                 console.log("Param error: ",param," -> ",data[param]);
+                param_missing=true;
             }
 
         }
@@ -20,16 +29,49 @@ const watchdog = function(configobj,currentdata){
         return false;
     }
 
-    function check_connection(data){
+    check_connection(data) {
         
-        if (data.state!="connected"){
-            errors.push({"error":"Device not available","date":unixTimestamp()});
+        if (param_ok("state",data)) {
+
+            if (data.state!="connected"){
+                errors.push({"error":"Device not available","date":unixTimestamp()});
+            }
+
         }
 
     }
 
+    check_param_missing() {
 
-    function unixTimestamp (d=null) {  
+        if (param_missing) {
+            errors.push({"error":"Required params missing","date":unixTimestamp()});
+        }
+
+    }
+
+    check_fault_code(){
+
+        if (param_ok("CurrentFault",data)) {
+            let errarr=data["CurrentFault"].match(/0: OK/);
+            if (errarr.length!=4) {
+                errors.push({"error":"Fault code","date":unixTimestamp(),"info":data["CurrentFault"]});
+            }
+        }
+
+    }
+
+    check_numeric_value(param,min,max){
+
+        if (param_ok(param,data)) {
+            
+            if (data[param]<min || data[param]>max) {
+                errors.push({"error":"Param "+param+" value not in range","date":unixTimestamp(),"info":data[param]+" not between "+min+" - "+max });
+            }
+        }
+
+    }
+
+    unixTimestamp (d=null) {  
         let bd=Date.now();
         if (d!==null){
             bd=d;
@@ -39,4 +81,4 @@ const watchdog = function(configobj,currentdata){
 
 }
 
-exports.watchdog=watchdog;
+module.exports=watchdog;
