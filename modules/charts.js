41978b8e111bdffbc7acc33b8baa749f408131de
suletom <suletom@freemail.hu>
2023-06-07 16:06:32 +0200

    nothing

--- modules/charts.js
+++ modules/charts.js
@@ -54,8 +54,8 @@ class charts{
 
           if (history.length>0){
 
-            return _getchart(
-            {
+            return _getchart(history
+            /*{
             "start_time": history[0]["timestamp"],
             "ah_min_point": 44,
             "ah_switch_point": 22,
@@ -68,7 +68,7 @@ class charts{
             "ah_capacity": history[history.length-1]["battery_capacity_ah"],
             "self_consumption_a": 1.1,
             "charge_max_a": history[history.length-1]["MaxChargerCurrent"] 
-            }
+            }*/
             );
           }
         }
@@ -242,11 +242,180 @@ class charts{
         return _graph(graphdata,graphbattsoc,graphconsumption,annot);
     }
 
-    static _graph=function(graphdata,graphbattsoc,graphconsumption,annot){
+    static _getchart=function(history){
+
+      
+
+      let start_time="";
+      for(let cv=0;cv<history.length;cv++){
+        if (start_time===""){
+          start_time=history[cv]["timestamp"];
+        }
+      }
+         
+      let prediction=forecast.getforecast();
+
+      let first_date="";
+      let last_date="";
+      for(let key in prediction.result.watts) {
+
+          if (first_date=="") first_date=helper.unixTimestamp(new Date(key));
+          last_date=helper.unixTimestamp(new Date(key));
+
+      }
+
+      let show_time=first_date;
+      if (!isNaN(parseInt(start_time))){
+         show_time=parseInt(start_time);
+      }
+
+      let energy=new energy1();
+
+      let graphdata=[];
+      let graphbattsoc=[];
+      let graphconsumption=[];
+
+      //run model from first to last date
+      let mode="";
+      let chargemode="";
+      let annot=[];
+      let modcol={"SBU":"#20b024","UTI":"#775DD0","OSO":"#ced10a","SNU":"#e62929"};
+
+      let max_main=0;
+      let min_main=Infinity;
+
+      let stepping=120; //sec
+
+      for(let t=first_date;t<last_date;t=t+stepping){
+
+          let newmode=energy.run(t,prediction,data.ah_min_point,data.ah_switch_point,data.ah_charge_point,data.current_ah,data.current_mode,data.ah_capacity,data.current_charge,data.consumption_a,data.voltage,data.charge_max_a);
+          if (max_main<newmode.predicted_data){
+            max_main=newmode.predicted_data;
+          }
+          if (min_main>newmode.predicted_data){
+            min_main=newmode.predicted_data;
+          }
+
+      }
+
+      for(let t=first_date;t<last_date;t=t+stepping){
+          
+          let newmode=energy.run(t,prediction,data.ah_min_point,data.ah_switch_point,data.ah_charge_point,data.current_ah,data.current_mode,data.ah_capacity,data.current_charge,data.consumption_a,data.voltage,data.charge_max_a);
+
+          if (helper.unixTimestamp()<t && helper.unixTimestamp()>t-stepping){
+            let now={
+              "x": t*1000,
+              "borderColor": "#000",
+              "label": {
+                "borderColor": "#000",
+                "style": {
+                  "color": "#fff",
+                  "background": "#222",
+                },
+                "text": "NOW"
+              }
+            };
+            annot.push(now);
+          }  
+          
+          if (t>show_time) {
+
+            if (mode!=newmode.suggested_mode){
+              let ob={
+                "x": t*1000,
+                "borderColor": modcol[newmode.suggested_mode],
+                "label": {
+                  "borderColor": modcol[newmode.suggested_mode],
+                  "style": {
+                    "color": "#fff",
+                    "background": modcol[newmode.suggested_mode],
+                  },
+                  "text": newmode.suggested_mode+": "+data.current_ah.toFixed(4)+"-"+(data.consumption_a*data.voltage)
+                }
+              };
+              annot.push(ob);
+              
+            }
+            if (chargemode!=newmode.suggested_charge){
+              let ob1={
+                "x": t*1000,
+                "borderColor": modcol[newmode.suggested_charge],
+                "label": {
+                  "borderColor": modcol[newmode.suggested_charge],
+                  "style": {
+                    "color": "#fff",
+                    "background": modcol[newmode.suggested_charge],
+                  },
+                  "text": newmode.suggested_charge,
+                  "offsetY": -38
+                }
+              };
+              annot.push(ob1);
+            }
+            
+            mode=newmode.suggested_mode;
+            chargemode=newmode.suggested_charge;
+          }  
+
+          graphdata.push([t*1000,newmode.predicted_data]);
+          graphbattsoc.push([t*1000, Math.round((data.current_ah/data.ah_capacity)*100) ]);
+          graphconsumption.push([t*1000, 
+            (mode=="SBU"?((data.consumption_a+data.self_consumption_a)*data.voltage):(data.self_consumption_a*data.voltage))]);
+
+          if (t>show_time) {
+            //simulate dischare/charge for next cycle..
+            let new_current_ah=data.current_ah;
+            if (mode=="SBU"){
+              //calculated consuption
+              new_current_ah+=(-(stepping/3600)*data.consumption_a);
+              new_current_ah+=(-(stepping/3600)*data.self_consumption_a);
+            }else{
+              if (mode=="UTI"){
+                //only self consuption
+                new_current_ah+=(-(stepping/3600)*data.self_consumption_a);
+              }
+            }
+                        
+            let charge=0;
+
+            if (chargemode=="SNU"){
+              charge=(stepping/3600)*data.charge_max_a;
+            }else{
+              charge=(( newmode.predicted_data/data.voltage));
+
+              //limit charger amps
+              if (charge>data.charge_max_a){
+                charge=data.charge_max_a;
+              }
+
+              charge=charge*(stepping/3600);
+            }
+
+            //solar charge
+            new_current_ah+=charge;
+
+            data.current_ah=new_current_ah;
+
+            if (data.current_ah>data.ah_capacity){
+              data.current_ah=data.ah_capacity;
+            }
+            if (data.current_ah<0){
+              data.current_ah=0;
+            }
+
+            data.current_mode=mode;
+            data.current_charge=chargemode;
+          }  
+      }
+           
+      return _graph(graphdata,graphbattsoc,graphconsumption,annot);
+  }
+
+    static _graph=function(id,graphdata,graphbattsoc,graphconsumption,annot=[]){
 
           let out=`<script>
 
-          function graph(){
+          function graph_${id}(){
 
             let graphdata1=${JSON.stringify(graphdata)};
             let battsoc1=${JSON.stringify(graphbattsoc)};
@@ -342,13 +511,13 @@ class charts{
           }
         };
 
-        let chart = new ApexCharts(document.querySelector("#charttest1"), options);
+        let chart = new ApexCharts(document.querySelector("#chart_${id}"), options);
         chart.render();
 
       }();
       </script>
 
-      <div id="charttest1"></div>`;
+      <div id="chart_${id}"></div>`;
 
       return out;
     }
