dcb022a3a2da59839aa932917919c06bbce7426e
suletom <suletom@freemail.hu>
2023-09-12 17:13:11 +0200

    model extra logging2

--- modules/energy_ver1.js
+++ modules/energy_ver1.js
@@ -128,10 +128,10 @@ class energyver1 {
 
           //200   4200/24 (168) 168+44
           if (this.current_ah > ((consumption_wh/this.voltage)+min_ah_to_store) ){
-              console.log("ENERGYv1: charge_enough: true (current_ah: "+this.current_ah+" calculated_ah: "+((consumption_wh/this.voltage)+min_ah_to_store)+")");
+              console.log("ENERGYv1: charge_enough: true (current_ah: "+this.current_ah+" calculated_ah: "+((consumption_wh/this.voltage)+min_ah_to_store)+")  min_ah_to_store: "+min_ah_to_store);
               return true;
           }else{
-              console.log("ENERGYv1: charge_enough: false (current_ah: "+this.current_ah+" calculated_ah: "+((consumption_wh/this.voltage)+min_ah_to_store)+")");
+              console.log("ENERGYv1: charge_enough: false (current_ah: "+this.current_ah+" calculated_ah: "+((consumption_wh/this.voltage)+min_ah_to_store)+") min_ah_to_store: "+min_ah_to_store);
               return false;
           }
           
